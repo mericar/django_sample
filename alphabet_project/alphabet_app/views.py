560a169cf93e2dc73bfae658e1b5a3c7391aa5ec
Mirko <mirkocarich@gmail.com>
2023-08-20 11:31:06 -0400

    notes

--- alphabet_project/alphabet_app/views.py
+++ alphabet_project/alphabet_app/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import string
 
+#Simple get method.
 @api_view(['GET'])
 def get_letter_detail(request, letter):
     if request.method == 'GET':
